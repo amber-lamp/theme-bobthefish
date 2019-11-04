ed7578c4bfb911959da5fb5f57d48309e8048464
agocover <inukaitai4@gmail.com>
2019-11-05 03:07:18 +0900

    update right-prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,79 +1,25 @@
-# You can override some default right prompt options in your config.fish:
-#     set -g theme_date_format "+%a %H:%M"
+function kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    return
+  end
 
-function __bobthefish_cmd_duration -S -d 'Show command duration'
-    [ "$theme_display_cmd_duration" = "no" ]
-    and return
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    return
+  end
 
-    [ -z "$CMD_DURATION" -o "$CMD_DURATION" -lt 100 ]
-    and return
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
 
-    if [ "$CMD_DURATION" -lt 5000 ]
-        echo -ns $CMD_DURATION 'ms'
-    else if [ "$CMD_DURATION" -lt 60000 ]
-        __bobthefish_pretty_ms $CMD_DURATION s
-    else if [ "$CMD_DURATION" -lt 3600000 ]
-        set_color $fish_color_error
-        __bobthefish_pretty_ms $CMD_DURATION m
-    else
-        set_color $fish_color_error
-        __bobthefish_pretty_ms $CMD_DURATION h
-    end
-
-    set_color $fish_color_normal
-    set_color $fish_color_autosuggestion
-
-    [ "$theme_display_date" = "no" ]
-    or echo -ns ' ' $__bobthefish_left_arrow_glyph
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
 end
 
-function __bobthefish_pretty_ms -S -a ms -a interval -d 'Millisecond formatting for humans'
-    set -l interval_ms
-    set -l scale 1
-
-    switch $interval
-        case s
-            set interval_ms 1000
-        case m
-            set interval_ms 60000
-        case h
-            set interval_ms 3600000
-            set scale 2
-    end
-
-    switch $FISH_VERSION
-        case 2.0.\* 2.1.\* 2.2.\* 2.3.\*
-            # Fish 2.3 and lower doesn't know about the -s argument to math.
-            math "scale=$scale;$ms/$interval_ms" | string replace -r '\\.?0*$' $interval
-        case 2.\*
-            # Fish 2.x always returned a float when given the -s argument.
-            math -s$scale "$ms/$interval_ms" | string replace -r '\\.?0*$' $interval
-        case \*
-            math -s$scale "$ms/$interval_ms"
-            echo -ns $interval
-    end
-end
-
-function __bobthefish_timestamp -S -d 'Show the current timestamp'
-    [ "$theme_display_date" = "no" ]
-    and return
-
-    set -q theme_date_format
-    or set -l theme_date_format "+%c"
-
-    echo -n ' '
-    date $theme_date_format
-end
-
-function fish_right_prompt -d 'bobthefish is all about the right prompt'
-    set -l __bobthefish_left_arrow_glyph \uE0B3
-    if [ "$theme_powerline_fonts" = "no" ]
-        set __bobthefish_left_arrow_glyph '<'
-    end
-
-    set_color $fish_color_autosuggestion
-
-    __bobthefish_cmd_duration
-    __bobthefish_timestamp
-    set_color normal
+function fish_right_prompt
+  echo (kubectl_status)
 end
